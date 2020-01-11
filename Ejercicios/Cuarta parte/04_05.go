9e412d3a1e0667fb020ee817fea93066726ab01b
EmiNu5 <emilianojaviernuniez@gmail.com>
2020-01-11 18:01:18 -0300

    refactorizado

--- Ejercicios/Cuarta parte/04_05.go
+++ Ejercicios/Cuarta parte/04_05.go
@@ -5,12 +5,9 @@ import "fmt"
 func main() {
 
 	var numero1 int = 78
-	var numero2 int = 6
-	var total int
+	var numero2 int = 4949
 
-	total = sumar(numero1, numero2)
-
-	fmt.Print(total)
+	fmt.Print(sumar(numero1, numero2))
 }
 func sumar(numero1 int, numero2 int) int {
 	var total int

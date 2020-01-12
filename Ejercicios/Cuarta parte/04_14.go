b044a6e57ff242698b0188c92488471da5937d73
EmiNu5 <emilianojaviernuniez@gmail.com>
2020-01-11 21:00:49 -0300

    nuevo archivo

---
+++ Ejercicios/Cuarta parte/04_14.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 14. Construir una función que dado un string devuelva el mismo string pero sin espacios por detrás. (nombrarla rightTrim)
+
+	var unString string = "hola  mundo como estas?  "
+	fmt.Print(rightTrim(unString), "final")
+}
+func rightTrim(unString string) string {
+	var i int
+	var nuevoString []byte
+	var j int
+
+	for i = len(unString) - 1; i >= 0; i-- {
+		if unString[i] != ' ' {
+			break
+		}
+	}
+	for j = 0; j <= i; j++ {
+		nuevoString = append(nuevoString, unString[j])
+	}
+
+	return string(nuevoString)
+}

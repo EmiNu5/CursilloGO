ca7b0478ce31e28d682726d65419be74f6034747
EmiNu5 <emilianojaviernuniez@gmail.com>
2020-01-14 20:00:55 -0300

    nuevo ejercicio

---
+++ Ejercicios/Cuarta parte/04_18.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// 18. Construir una función que dado un string y un byte devuelva
+	//  un entero que represente el índice de la primer ocurrencia de ese byte en el string.
+	//   Si el byte no se encuentra devolver -1. (nombrarla findFirstChar).
+
+	var unString string = "programar es divertido"
+	var unChar byte = 'd'
+
+	fmt.Println(findFirstChar(unString, unChar))
+}
+func findFirstChar(unString string, unChar byte) int {
+	var i int
+
+	for i = 0; i < len(unString); i++ {
+		if unString[i] == unChar {
+			return i - 1
+			break
+		}
+	}
+	return -1
+}

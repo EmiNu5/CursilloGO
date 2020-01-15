4a90b162073965619f6d0870cf60445bb4d689fb
EmiNu5 <emilianojaviernuniez@gmail.com>
2020-01-14 21:38:28 -0300

    nuevo

---
+++ Ejercicios/Cuarta parte/04_19.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 19. Construir una función que dado un string y un byte devuelva
+	//  un slice de enteros que represente todos los índices con las ocurrencias de ese byte en el string
+	//  (nombrarla findAllChar).
+	var unString string = "jijiji"
+	var unChar byte = 'i'
+
+	fmt.Printf("%s", findAllChar(unString, unChar))
+}
+func findAllChar(unString string, unChar byte) string {
+	var unSlice []int
+	var i int
+
+	for i = 0; i < len(unString); i++ {
+		if unString[i] == unChar {
+			unSlice = append(unSlice, i)
+		}
+	}
+
+	return fmt.Sprint(unSlice)
+}

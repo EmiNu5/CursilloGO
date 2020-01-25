d76076694078850396164068370100980c141654
EmiNu5 <emilianojaviernuniez@gmail.com>
2020-01-25 15:48:43 -0300

    eureka

--- Ejercicios/Cuarta parte/04_20.go
+++ Ejercicios/Cuarta parte/04_20.go
@@ -7,20 +7,32 @@ func main() {
 	//  un entero que represente el índice de la primer ocurrencia del segundo string en el primero.
 	//   Si el string no se encuentra devolver -1. (nombrarla findFirstString).
 	var primerString string = "programar"
-	var segundoString string = "divertido"
+	var segundoString string = "mar"
 
 	fmt.Println(findFirstString(primerString, segundoString))
 }
 func findFirstString(primerString string, segundoString string) int {
 	var i int
 	var j int
+	var coincidencia int = 0
+	var indice int = -1
+
 	for i = 0; i < len(primerString); i++ {
-		for j = 0; j < len(segundoString); j++ {
+		fmt.Println("indice", i)
+		for ; j < len(segundoString); j++ {
 			if primerString[i] == segundoString[j] {
-				return j
+				coincidencia++
+			} else {
 				break
 			}
 		}
+		fmt.Println("coincidencia", coincidencia)
+		if coincidencia == len(segundoString) {
+			fmt.Println(coincidencia, "==", len(segundoString))
+			indice = len(primerString) - coincidencia
+			break
+		}
+
 	}
-	return -1
+	return indice
 }

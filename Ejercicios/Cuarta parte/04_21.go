bf591a930cd6750dac968b46c634eb5efaa2013b
EmiNu5 <emilianojaviernuniez@gmail.com>
2020-02-02 21:02:22 -0300

    nuevo ejercicio

---
+++ Ejercicios/Cuarta parte/04_21.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+//21. Construir una función que dados dos strings
+// devuelva un slice de enteros que contengan los índices de todas las ocurrencias
+// del segundo string en el primero (nombrarla findAllString).
+
+func main() {
+	var firstString string = "papapas"
+	var secondString string = "papapa"
+
+	fmt.Println(findAllString(firstString, secondString))
+}
+func findAllString(firstString string, secondString string) []int {
+	var i int
+	var j int
+	var found bool
+	var ocurrence []int = []int{}
+
+	for i = 0; i < len(firstString); i++ {
+		found = true
+		for j = 0; j < len(secondString); j++ {
+			if j+i >= len(firstString) || firstString[i+j] != secondString[j] {
+				found = false
+				break
+			}
+		}
+		if found {
+			ocurrence = append(ocurrence, i)
+		}
+	}
+	return ocurrence
+}

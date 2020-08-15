0f2b6e0eff637b5ecfe0d4315de61dba03a2b1e4
EmiNu5 <emilianojaviernuniez@gmail.com>
2020-08-15 19:11:18 -0300

    comentamos

--- Ejercicios/Tercera parte/Tercera tanda/03_03_10.go
+++ Ejercicios/Tercera parte/Tercera tanda/03_03_10.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
 	var i int
 	var j int
 	var k int
@@ -15,18 +15,18 @@ func main(){
 	fmt.Println("Ingresar un string")
 	fmt.Scan(&string1)
 
-	fmt.Println("Ingresar los",cant," strings")
+	fmt.Println("Ingresar loss", cant, " strings")
 
-	for i=0;i<cant;i++{
+	for i = 0; i < cant; i++ {
 		fmt.Scan(&strings[i])
 	}
 
-	for i=0;i<len(strings);i++{
+	for i = 0; i < len(strings); i++ {
 		stringelegido = strings[i]
-		for j=0;j<len(string1);j++{
+		for j = 0; j < len(string1); j++ {
 			coincidencia = true
-			for k=0;k<len(stringelegido);k++{
-				if stringelegido[k] != string1[j+k]{
+			for k = 0; k < len(stringelegido); k++ {
+				if stringelegido[k] != string1[j+k] {
 					coincidencia = false
 					break
 				}
@@ -35,7 +35,7 @@ func main(){
 				break
 			}
 		}
-		if coincidencia{
+		if coincidencia {
 			break
 		}
 	}
